956a2e8024dd3884b9a238cfebea7d7e847133ac
djcupo <djcupo@AK120D-04.admin.wpi.edu>
2017-01-24 21:58:23 -0500

    Removed multi imports and fixed a print statement

--- Lab 1/Lab1_ADCandWaveform.c
+++ Lab 1/Lab1_ADCandWaveform.c
@@ -5,41 +5,6 @@
  *      Author: Mike
  */
 
-
-/* main.c
- *
- *  Created on: January 22nd, 2017
- *      Author: mphopkins
- */
-
-
-#include "Lab1_ADCandWaveform.h"
-
-
-//character for receiving serial data
-char inchar;
-int lowADC = 0;
-int highADC=1023;
-int tempValConversion = 69;
-int compareCounter = F100HZ;
-int outputState = 0;
-double dutyCycle = 20;
-volatile int timerCount = 0;
-/*
- * Lab1_ADCandWaveform.c
- *
- *  Created on: Jan 23, 2017
- *      Author: Mike
- */
-
-
-/* main.c
- *
- *  Created on: August 20, 2016
- *      Author: Joest
- */
-
-
 #include "RBELib/RBELib.h"
 //For use of abs()
 #include <stdlib.h>
@@ -82,103 +47,12 @@ void sample()
 	//print samples here
 	for(int i = 0; i<250;i++)
 	{
-		printf("%i, %i", i,samples[i]);
+		printf("Index is %i, Value is %i", i,samples[i]);
 	}
 	TCCR0B = 0b00000011;
 	timerCount=0;
 }
 
-int main(void)
-{
-	initRBELib();
-
-	//Timer Setup
-	TIMSK0 = 0b10; //Enable Interrupt
-	TCCR0A=0; //Pin setting and waveform gen setting
-	TCCR0B = 0b00000011; //sets clock to (clk/8)  // 001 = clk/1
-	OCR0A = 18; //the compare register not supposed to be binary
-	sei();
-
-
-	DDRD = 0xFF;
-	PORTD = 0b00001111;
-	DDRA = 0xFF;
-	PORTA = 0b11111111;
-
-	//Setup Switches
-
-	DDRB = 0; //Port B all to input
-	PORTB = 1; //pullup on pin 0
-
-
-	//Init ADC
-	ADMUX =  0b01100111;
-	ADCSRA = 0b11100000;
-	ADCSRB = 0b00000100;
-	ADCSRA |= 0x40; //Starts conversion
-	while(1)
-	{
-		if(~PINB & 0b1) //if PORT B0 is low change frequency
-		{
-			compareCounter = F1HZ;
-		}
-
-		if(~PINB & 0b10) //if PORT B1 is low change frequency
-		{
-			compareCounter = F20HZ;
-		}
-
-		if(~PINB & 0b100) //if PORT B2 is low change frequency
-		{
-			compareCounter = F100HZ;
-		}
-
-		if(~PINB & 0b1000) //if PORT B3 is low start sampling
-		{
-			sample();
-		}
-
-		if(outputState == 0 && timerCount > (compareCounter *2.0 * (100.0 - dutyCycle))/100.0)
-		{
-			timerCount = 0;
-			TCNT0=0;
-			PORTA = 0xFF;
-			outputState = 1;
-		}
-
-		if(outputState == 1 && timerCount > (compareCounter *2.0 * dutyCycle)/100.0)
-		{
-			timerCount = 0;
-			TCNT0=0;
-			PORTA = 0;
-			outputState = 0;
-		}
-
-
-		if(ADCSRA & 0x10) //if conversion is complete
-		{
-
-			dutyCycle = ADCH;
-			printf("%d",dutyCycle);
-			dutyCycle = dutyCycle * 0.08 + 10;
-			if(dutyCycle < 10)
-			{
-				dutyCycle = 10;
-			}
-			if(dutyCycle > 90)
-			{
-				dutyCycle = 90;
-			}
-
-		}
-	}
-}
-
-ISR(TIMER0_COMPA_vect)
-{
-	timerCount++;
-	TCNT0=0;
-}
 
 
 void Lab1init(void){
