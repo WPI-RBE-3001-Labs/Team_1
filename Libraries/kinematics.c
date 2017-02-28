d9124f93dc48bb22f66a3f718bf7b304fdcd7de6
MikeHopkins <mphopkins@wpi.edu>
2017-02-27 21:04:25 -0500

    I think I found the problem and yes it was 1 "(" that was off by 1 term

--- Libraries/kinematics.c
+++ Libraries/kinematics.c
@@ -31,7 +31,7 @@ int signTheta2(double x,double y)
 double xyToTheta1(double x, double y,double theata2)
 {
 	//double theata2 = xyToTheta2(x,y);
-	return (atan2(LENGTH2*sin(theata2)*x+LENGTH1+(LENGTH2*cos(theata2)*y),(LENGTH1+LENGTH2*cos(theata2))*x-LENGTH2*sin(theata2)*y))*(180/3.1415);
+	return (atan2(LENGTH2*sin(theata2)*x+(LENGTH1+LENGTH2*cos(theata2))*y,(LENGTH1+LENGTH2*cos(theata2))*x-LENGTH2*sin(theata2)*y))*(180/3.1415);
 }
 
 double xyToTheta2(double x, double y)
